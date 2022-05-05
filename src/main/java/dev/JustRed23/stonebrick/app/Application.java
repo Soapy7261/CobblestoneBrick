86fe606b5ac4d35e6bd1157568f0dc984787cc57
justred23 <manuel.bormans@gmail.com>
2022-05-05 10:18:00 +0200

    Add base application classes

---
+++ src/main/java/dev/JustRed23/stonebrick/app/Application.java
@@ -0,0 +1,7 @@
+package dev.JustRed23.stonebrick.app;
+
+public interface Application {
+    void init() throws Exception;
+    void start() throws Exception;
+    void stop() throws Exception;
+}
---
+++ src/main/java/dev/JustRed23/stonebrick/app/ConsoleApp.java
@@ -0,0 +1,6 @@
+package dev.JustRed23.stonebrick.app;
+
+public abstract class ConsoleApp implements Application {
+
+    public abstract void start() throws Exception;
+}
