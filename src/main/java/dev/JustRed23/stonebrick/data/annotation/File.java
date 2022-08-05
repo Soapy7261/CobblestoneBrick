2b3bf2136fe311637da3427aa58fe5081b539d68
JustRed23 <manuel.bormans@gmail.com>
2022-08-05 22:18:25 +0200

    Start file structure: add base annotations

---
+++ src/main/java/dev/JustRed23/stonebrick/data/annotation/File.java
@@ -0,0 +1,14 @@
+package dev.JustRed23.stonebrick.data.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface File {
+    String name();
+    String directory() default "";
+    String content() default "";
+}
